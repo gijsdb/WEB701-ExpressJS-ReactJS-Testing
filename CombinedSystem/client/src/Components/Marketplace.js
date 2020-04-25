b8dbfdf94db2e2ca6abc024685edb4c9e5fb5700
gijsdb <gijsdeblauw@gmail.com>
2020-04-25 13:37:00 +1200

    added redux-persist to maintain store while refreshing page

--- CombinedSystem/client/src/Components/Marketplace.js
+++ CombinedSystem/client/src/Components/Marketplace.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import axios from 'axios'
 
 class Marketplace extends React.Component {
+
+    componentDidMount() {
+
+    }
+
     render() {
         return (
-          <div className="container padTop center">
-              <h1>Marketplace</h1>
-          </div>
+            <div className="container padTop">
+            <div className="row">
+            <div className="col">
+                <h1>Marketplace</h1>
+            </div>
+            </div>
+            {/* Add new product component here */}
+            <div className="row padTop">
+                <div className="col">
+                    <h1>Hops stored in database</h1>
+                </div>
+            </div>
+
+            <div className="row padBottom">
+            <div className="col-md-4">
+                <div className="card hopCard">
+                <img src="../assets/isolatedHop.png" className="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title"></h5>
+                    <p className="card-text"> Bitterness: </p>
+                    <p className="card-text"> Sweetness: </p>
+                    <p className="card-text"> Weight: </p>
+                    <p className="card-text"> Price: </p>
+                    <p className="card-text"> Added by:</p>
+                </div>
+                </div>
+            </div>
+            </div>
+            
+        </div>
         );
       }
 }
--- CombinedSystem/client/src/index.js
+++ CombinedSystem/client/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import { createStore, applyMiddleware, compose } from 'redux'
 import {Provider} from 'react-redux'
 import thunk from 'redux-thunk'
-
 import allReducers from './Reducers/index'
+
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -19,18 +23,36 @@ const store = createStore(
   // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 */
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, allReducers)
+
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-export const store = createStore(allReducers, /* preloadedState, */ composeEnhancers(
+export const store = createStore(persistedReducer, composeEnhancers(
     applyMiddleware(thunk)
 ));
 
+
+// export const store = compose(persistedReducer, {}, composeEnhancers(applyMiddleware(thunk)));
+
+
+
+export const persistor = persistStore(store);
+
 const rootElement = document.getElementById('root')
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+          <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
   rootElement
 );
 
